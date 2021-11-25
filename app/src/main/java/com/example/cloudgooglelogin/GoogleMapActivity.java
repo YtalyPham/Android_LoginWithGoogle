586da832317fa07493921f65b4cde0fb026c3dba
YtalyPham <nhuyphamdev@gmail.com>
2021-11-25 09:14:16 +0700

    CompleteMark Position

--- app/src/main/java/com/example/cloudgooglelogin/GoogleMapActivity.java
+++ app/src/main/java/com/example/cloudgooglelogin/GoogleMapActivity.java
@@ -3,37 +3,47 @@ package com.example.cloudgooglelogin;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GoogleMapActivity extends AppCompatActivity{
+public class GoogleMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_map);
 
-        Fragment fragment= new MapFragment();
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.frame_layout,fragment)
-                .commit();
+
+        SupportMapFragment mapFragment= (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.frame_layout);
+
+        mapFragment.getMapAsync(this);
+
+
 
 
     }
-    //                        //10.778469265452536, 106.69579088412257
-//                        map=googleMap;
-//                        LatLng HCM=new LatLng(10.778469265452536, 106.69579088412257);
-//                        map.addMarker(new MarkerOptions().position(HCM).title("Ho Chi Minh"));
-//                        map.moveCamera(CameraUpdateFactory.newLatLng(HCM));
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+                                //10.778469265452536, 106.69579088412257
+                        map=googleMap;
+                        LatLng HCM=new LatLng(10.778469265452536, 106.69579088412257);
+                        map.addMarker(new MarkerOptions().position(HCM).title("Ho Chi Minh"));
+                        map.moveCamera(CameraUpdateFactory.newLatLng(HCM));
+
+    }
 
 
 
